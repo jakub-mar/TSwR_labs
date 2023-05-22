c4ccd4f57e80d9555085dbed58839df4ef2583b2
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2023-05-22 12:12:48 +0200

    fixed invalid parameters in mmac model contructor

--- controllers/mma_controller.py
+++ controllers/mma_controller.py
@@ -9,9 +9,9 @@ class MMAController(Controller):
         # I:   m3=0.1,  r3=0.05
         # II:  m3=0.01, r3=0.01
         # III: m3=1.0,  r3=0.3
-        self.model1 = ManiuplatorModel(Tp, 0.05, 0.1)
+        self.model1 = ManiuplatorModel(Tp, 0.1, 0.05)
         self.model2 = ManiuplatorModel(Tp, 0.01, 0.01)
-        self.model3 = ManiuplatorModel(Tp, 0.3, 1.0)
+        self.model3 = ManiuplatorModel(Tp, 1.0, 0.3)
 
         # testowe
         # self.model1 = ManiuplatorModel(Tp, 0.0001, 0.1)
@@ -32,8 +32,6 @@ class MMAController(Controller):
             if error < lastError:
                 lastError = error
                 self.i = index
-
-        print("############################", self.i)
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         self.choose_model(x)
